59e8505a73cd9611fa44c7b747e70628f82d62c9
Zoltan Nagy <abesto0@gmail.com>
2017-06-12 12:14:22 +0200

    [prometheus] Add zipkin_collector metrics relabeling

--- prometheus/create-datasource-and-dashboard.sh
+++ prometheus/create-datasource-and-dashboard.sh
@@ -11,8 +11,10 @@ fi
 dashboard_id=1598
 last_revision=$(curl -sf https://grafana.com/api/dashboards/${dashboard_id}/revisions | grep '"revision":' | sed 's/ *"revision": \([0-9]*\),/\1/' | sort -n | tail -1)
 
-curl -s https://grafana.com/api/dashboards/${dashboard_id}/revisions/${last_revision}/download | \
-    xargs -0 -I "{}" curl --retry-connrefused --retry 5 --retry-delay 0 -sf \
-          -X POST -H "Content-Type: application/json" \
-          --data-binary '{"dashboard": {}, "inputs": [{"name": "DS_PROM", "pluginId": "prometheus", "type": "datasource", "value": "prom"}], "overwrite": false}' \
-          http://grafana:3000/api/dashboards/import
+echo '{"dashboard": ' > data.json
+curl -s https://grafana.com/api/dashboards/${dashboard_id}/revisions/${last_revision}/download >> data.json
+echo ', "inputs": [{"name": "DS_PROM", "pluginId": "prometheus", "type": "datasource", "value": "prom"}], "overwrite": false}' >> data.json
+curl --retry-connrefused --retry 5 --retry-delay 0 -sf \
+     -X POST -H "Content-Type: application/json" \
+     --data-binary @data.json \
+     http://grafana:3000/api/dashboards/import
